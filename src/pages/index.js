e768a50917a63fea0dfaa823ea4a67ef5cf3c173
shakunage <raraachan1111@gmail.com>
2026-06-17 19:53:43 +0900

    Customize top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,34 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Link from '@docusaurus/Link';
 import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      title="がじゅまるIsland Wiki"
+      description="がじゅまるIslandの公式Wiki">
+      <header className="hero hero--primary">
+        <div className="container">
+          <Heading as="h1" className="hero__title">
+            がじゅまるIsland Wiki
+          </Heading>
+          <p className="hero__subtitle">
+            釣って、作って、旅をする。
+          </p>
+          <div>
+            <Link className="button button--secondary button--lg" to="/docs/intro">
+              Wikiを見る
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      <main className="container margin-vert--xl">
+        <Heading as="h2">現在整備中です</Heading>
+        <p>
+          ルール、強化要素、乗り物、Kingdoms、レアドロップなどの情報を順次追加していきます。
+        </p>
       </main>
     </Layout>
   );
